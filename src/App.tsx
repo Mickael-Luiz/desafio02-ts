f0355aa2e5be4e92fadc14ecf83a6068dc4b2c43
Mickael Dias <mickaeldias00@gmail.com>
2024-02-07 11:09:01 -0400

    ajuste

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,22 @@
+import { useState } from 'react';
 import { ChakraProvider, Box } from '@chakra-ui/react'
-import { Header } from './components/Header/Header';
-import { Footer } from './components/Footer/Footer';
+
 import { Card } from './components/Card/Card';
+import { Layout } from './components/Layout';
 
 function App() {
+
+  const [value, setValue] = useState(0)
+
+  console.log(value);
+
   return (
     <ChakraProvider>
       <Box minHeight='100vh' backgroundColor='#111133' display='flex' flexDirection='column' justifyContent='space-between'>
 
-        <Header />
-        <Card />
-        <Footer />
+        <Layout>
+          <Card />
+        </Layout>
 
       </Box>
     </ChakraProvider>
